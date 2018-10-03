5001b9217b0a3dcd0b85f57bc1f524ab39820d16
Iris <i.borkovsky@gmail.com>
2018-10-02 19:02:10 -0700

    String Validators
    Use python's build in string validation methods to check if a given string contains certain characters.

---
+++ python_practie_problems/strings/any_checker.py
@@ -0,0 +1,12 @@
+# You are given a string 's' 
+# Your task is to find out if the string contains: alphanumeric characters, alphabetical characters, digits, lowercase and uppercase characters.
+
+
+
+def checker(string):
+	string.isalnum()
+	string.isalpha()
+	string.isdigit()
+	string.islower()
+	string.isupper()
+

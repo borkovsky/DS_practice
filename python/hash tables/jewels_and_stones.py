69bb1157c4f4de0a3cf4da652ad8d5667f95edee
Iris <i.borkovsky@gmail.com>
2018-10-11 16:04:09 -0700

    Jewels and Stones Leet Code problem

--- python/hash tables/jewels_and_stones.py
+++ python/hash tables/jewels_and_stones.py
@@ -2,7 +2,8 @@
 # https://leetcode.com/problems/jewels-and-stones/description/
 
 # # You're given strings J representing the types of stones that are jewels, 
-# and S representing the stones you have.  Each character in S is a type of stone you have.  
+# and S representing the stones you have.  
+# Each character in S is a type of stone you have.  
 # You want to know how many of the stones you have are also jewels.
 
 # # The letters in J are guaranteed distinct, and all characters in J and S are letters. 
